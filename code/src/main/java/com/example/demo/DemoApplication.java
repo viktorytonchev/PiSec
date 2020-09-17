cae1020c20c08270aba15a42b2db6a53c187287d
vpopovs <vpibvp@gmail.com>
2020-09-17 14:51:14 +0200

    init

---
+++ code/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class DemoApplication implements Runnable {
+
+	public void run() {
+		System.out.println("Hello World!");
+	}
+}
---
+++ code/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+
+import org.junit.Test;
+
+public class DemoApplicationTests {
+
+	@Test
+	public void testHelloWorld(){
+		Thread app = new Thread(new DemoApplication());
+		app.start();
+	}
+}

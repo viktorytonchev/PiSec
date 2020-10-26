256bb11bcae3c7f4d29f9af03c45cf5bed7403f9
vpopovs <vpibvp@gmail.com>
2020-10-26 18:58:35 +0100

    armed/disarmed threaded monitor, implemented with locks

--- code/src/main/java/pi/sensor_control.py
+++ code/src/main/java/pi/sensor_control.py
@@ -1,22 +1,55 @@
-from threading import Thread
+from threading import Thread, Lock
 from time import sleep
 
 import RPi.GPIO as GPIO
 import psycopg2 as db
 
+from pi import security
+
+SYSTEM_STATE = None
+state_lock = Lock()
+
+def get_system_state():
+    conn = None
+    try:
+        conn = db.connect(
+            host=security.db_user,
+            database=security.db_database,
+            user=security.db_user,
+            password=security.db_password)
+        cur = conn.cursor()
+        cur.execute("SELECT s.armed FROM dab_di19202b_226.pisec.system s") # where sid = ...
+        record = cur.fetchone()
+        for row in record:
+            armed = row[0]
+            return armed
+    except (Exception, db.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def system_state_controller(arg):
+    while True:
+        sleep(0.01)
+        state_lock.acquire()
+        try:
+            SYSTEM_STATE = get_system_state()
+        finally:
+            state_lock.release()
 
 def main():
     GPIO.setmode(GPIO.BCM)
 
-    SENSOR_GPIO = 3
+    sensor_gpio = 3
 
-    GPIO.setup(SENSOR_GPIO, GPIO.IN)
+    GPIO.setup(sensor_gpio, GPIO.IN)
 
-    current = GPIO.input(SENSOR_GPIO)
+    current = GPIO.input(sensor_gpio)
 
     while True:
         sleep(0.01)
-        state = GPIO.input(SENSOR_GPIO)
+        state = GPIO.input(sensor_gpio)
         if state and not current:
             thread = Thread(target=update_sensor_status, args=(True,))
             thread.start()
@@ -26,15 +59,14 @@ def main():
             thread.start()
             current = False
 
-
 def update_sensor_status(arg):
     conn = None
     try:
         conn = db.connect(
-            host="bronto.ewi.utwente.nl",
-            database="dab_di19202b_226",
-            user="dab_di19202b_226",
-            password="ivanekuche")
+            host=security.db_user,
+            database=security.db_database,
+            user=security.db_user,
+            password=security.db_password)
         cur = conn.cursor()
 
         if arg:

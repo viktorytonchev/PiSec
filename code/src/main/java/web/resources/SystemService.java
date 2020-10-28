c749b3092ab058f8372f726d36a017a243657100
vikic <vikic200@gmail.com>
2020-10-28 22:55:14 +0100

    mainPage.html - Arming/Disarming of the System now works SystemService.java - Added supporting methods to arm/disarm the system and check if a pin is correct

--- code/src/main/java/web/resources/SystemService.java
+++ code/src/main/java/web/resources/SystemService.java
@@ -1,6 +1,7 @@
 package web.resources;
 
 import web.login.Security;
+import web.model.Email;
 import web.model.System;
 
 import javax.annotation.processing.Generated;
@@ -10,6 +11,18 @@ import java.sql.*;
 
 @Path("/system")
 public class SystemService {
+
+    public static boolean
+    onlyDigits(String str, int n)
+    {
+        boolean result = false;
+        for (int i = 0; i < n; i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                result = true;
+            }
+        }
+        return result;
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -51,5 +64,80 @@ public class SystemService {
 
         return result;
     }
+
+    @Path("{pin}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public boolean checkPin(@PathParam("pin") String pinString){
+        boolean result = false;
+
+        Connection conn;
+        String dbuser = Security.DB_USER;
+        String passwd = Security.DB_PASSWORD;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://bronto.ewi.utwente.nl:5432/" + dbuser,
+                    dbuser, passwd);
+            conn.setAutoCommit(true);
+            conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            String query = "SELECT s.pin FROM " + dbuser + ".pisec.system s";
+            PreparedStatement st = conn.prepareStatement(query);
+            ResultSet resultSet = st.executeQuery();
+            String pin = "placeholder";
+            while (resultSet.next()) {
+                pin = resultSet.getString("pin");
+            }
+
+            if(onlyDigits(pin, pin.length()) && pin.equals(pinString)){
+                result = true;
+            }
+
+            conn.close();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    @Path("arm")
+    @POST
+    @Consumes({MediaType.APPLICATION_FORM_URLENCODED})
+    public void armDisarmSystem(){
+
+        Connection conn;
+        String dbuser = Security.DB_USER;
+        String passwd = Security.DB_PASSWORD;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://bronto.ewi.utwente.nl:5432/" + dbuser,
+                    dbuser, passwd);
+            conn.setAutoCommit(true);
+            conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            String query = "SELECT s.armed FROM " + dbuser + ".pisec.system s" ;
+            PreparedStatement st = conn.prepareStatement(query);
+            ResultSet resultSet = st.executeQuery();
+
+            boolean armed = false;
+            while (resultSet.next()) {
+                armed = resultSet.getBoolean("armed");
+            }
+
+            query = "UPDATE " + dbuser + ".pisec.system SET armed = ?";
+            PreparedStatement st1 = conn.prepareStatement(query);
+            st1.setBoolean(1, !armed);
+            resultSet = st1.executeQuery();
+            resultSet.next();
+
+            conn.close();
+            }
+        catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+        }
+
+    }
+
+
 
 }
